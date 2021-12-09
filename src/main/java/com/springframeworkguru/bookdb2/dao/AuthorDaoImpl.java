57fd6032d1c4cafe372db7c3e234ab3488c6aaf8
RadekProvaznik <provazniksde@gmail.com>
2021-12-09 14:53:44 +0100

    get by id DAO

--- src/main/java/com/springframeworkguru/bookdb2/dao/AuthorDaoImpl.java
+++ src/main/java/com/springframeworkguru/bookdb2/dao/AuthorDaoImpl.java
@@ -3,10 +3,57 @@ package com.springframeworkguru.bookdb2.dao;
 import com.springframeworkguru.bookdb2.domain.Author;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+import java.sql.*;
+
 @Component
 public class AuthorDaoImpl implements AuthorDao {
+
+    private final DataSource source;
+
+    public AuthorDaoImpl(DataSource source) {
+        this.source = source;
+    }
+
     @Override
     public Author getById(Long id) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = source.getConnection();
+            ps = connection.prepareStatement("SELECT * FROM author WHERE id = ?");
+            ps.setLong(1, id);
+            resultSet = ps.executeQuery();
+
+            if (resultSet.next()) {
+               Author author = new Author();
+               author.setId(id);
+               author.setFirstName(resultSet.getString("first_name"));
+               author.setLastName(resultSet.getString("last_name"));
+
+               return author;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+
+                if (ps != null) {
+                    ps.close();
+                }
+
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
         return null;
     }
 }
--- src/test/java/com/springframeworkguru/bookdb2/AuthorDaoIntegrationTest.java
+++ src/test/java/com/springframeworkguru/bookdb2/AuthorDaoIntegrationTest.java
@@ -6,11 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
+@ComponentScan(basePackages = {"com.springframeworkguru.bookdb2.dao"})
 @ActiveProfiles("local")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class AuthorDaoIntegrationTest {
